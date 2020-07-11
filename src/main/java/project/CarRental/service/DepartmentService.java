1db754b47206116ea2fa1497d5e8eefaeca744fe
Ania Urban <cynglowa1@wp.pl>
2020-07-11 21:21:51 +0200

    problem z edytowaniem departments. Dodane: servis, contriller web dla Department

--- src/main/java/project/CarRental/service/DepartmentService.java
+++ src/main/java/project/CarRental/service/DepartmentService.java
@@ -2,7 +2,14 @@ package project.CarRental.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import project.CarRental.model.dto.DepartmentDto;
+import project.CarRental.model.entity.Department;
+import project.CarRental.model.mappers.DepartmentMapper;
 import project.CarRental.model.repository.DepartmentRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DepartmentService {
@@ -12,5 +19,31 @@ public class DepartmentService {
     @Autowired
     public DepartmentService(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
+    }
+
+    public List<DepartmentDto> getAllDepartment() {
+        Iterable<Department> departmentsList = departmentRepository.findAll();
+        ArrayList<DepartmentDto> result = new ArrayList<>();
+        for(Department department : departmentsList) {
+            result.add(DepartmentMapper.INSTANCE.departmentToDto(department));
+        }
+        return result;
+    }
+
+    public DepartmentDto getDepartmentById(Integer id) {
+        Optional<Department> department = departmentRepository.findById(id);
+        if(department.isPresent()) {
+            return DepartmentMapper.INSTANCE.departmentToDto(department.get());
+        }
+        return null;
+    }
+
+    public void saveDepartment(DepartmentDto departmentDto) {
+        Department department = DepartmentMapper.INSTANCE.dtoToDepartment(departmentDto);
+        departmentRepository.save(department);
+    }
+
+    public void deleteDepartmentById(Integer id) {
+        departmentRepository.deleteById(id);
     }
 }
---
+++ src/main/webapp/WEB-INF/editDepartment.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ania
+  Date: 2020-07-11
+  Time: 20:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Edytowanie oddziałów w bazie</title>
+</head>
+<body>
+<h1>Edytuj oddział</h1>
+<form method="post" action="/editDepartment">
+    Adres: <input name="depAddress" value="${department.depAddress}" /><br/>
+    <input type="submit" value="Zapisz">
+</form>
+
+</body>
+</html>
